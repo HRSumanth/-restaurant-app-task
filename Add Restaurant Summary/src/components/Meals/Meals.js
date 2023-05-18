1be9f6d1f2a6d9d0e2d51d9ecefc86e54aff4559
HRSumanth <130749610+HRSumanth@users.noreply.github.com>
2023-05-18 07:53:00 +0530

    Add files via upload

---
+++ Add Restaurant Summary/src/components/Meals/Meals.js
@@ -0,0 +1,17 @@
+import './Meals.css'
+
+import { Fragment } from 'react'
+
+import Summary from './Summary'
+
+const Meals = ()=>{
+  return (
+    <Fragment>
+        <Summary></Summary>
+    </Fragment>
+    
+  
+  )
+}
+
+export default Meals
